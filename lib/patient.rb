8d3d4924d072939ec6a2f871cabb3204e617035b
Adrian Camacho <adriancamacho18@gmail.com>
2021-07-27 16:12:47 -0700

    add 5 passing tests
    Co-authored-by: Dave Lindqvist <Lindqvist.Dave@gmail.com>

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,12 +1,12 @@
 class Patient
-  attr_accessor :name, :birthday
+  attr_accessor :name, :birthday, :doctor_id
   attr_writer
-  attr_reader :id, :doctor_id
+  attr_reader :id
 
   def initialize(attributes)
     @id = attributes.fetch(:id)
     @name = attributes.fetch(:name)
-    @dob = attributes.fetch(:birthday)
+    @birthday = attributes.fetch(:birthday)
     @doctor_id = attributes.fetch(:doctor_id)
   end
 
@@ -24,7 +24,7 @@ class Patient
   end
 
   def save
-    result = DB.exec("INSERT INTO patients (name, birthday) VALUES ('#{@name}', '#{@dob}') RETURNING id;")
+    result = DB.exec("INSERT INTO patients (name, birthday, doctor_id) VALUES ('#{@name}', '#{@birthday}', #{@doctor_id}) RETURNING id;")
     @id = result.first().fetch("id").to_i
   end
 
@@ -32,4 +32,34 @@ class Patient
     self.name() == patient_to_compare.name()
   end
 
+  # def albums
+  #   albums = []
+  #   results = DB.exec("SELECT album_id FROM albums_artists WHERE artist_id = #{@id};")
+  #   results.each() do |result|
+  #     album_id = result.fetch("album_id").to_i()
+  #     album = DB.exec("SELECT * FROM albums WHERE id = #{album_id};")
+  #     name = album.first().fetch("name")
+  #     albums.push(Album.new({:name => name, :id => album_id}))
+  #   end
+  #   albums
+  # end
+
+  # def update(attributes)
+  #   if (attributes.has_key?(:name)) && (attributes.fetch(:name) != nil)
+  #     @name = attributes.fetch(:name)
+  #     DB.exec("UPDATE patients SET name = '#{@name}' WHERE id = #{@id};")
+  #   elsif (attributes.has_key?(:doctor_name)) && (attributes.fetch(:doctor_name) != nil)
+  #     doctor_name = attributes.fetch(:doctor_name)
+  #     doctor = DB.exec("SELECT * FROM doctors WHERE lower(name)='#{doctor_name.downcase}';").first
+  #     if doctor != nil
+  #       DB.exec("INSERT INTO patients (doctor_id) VALUES (#{doctor['id'].to_i}, #{@id});")
+  #     end
+  #   end
+  # end
+
+  def update(attributes)
+    @name = name
+    @doctor_id = doctor_id
+    DB.exec("UPDATE patients SET name = '#{@name}', doctor_id = #{@doctor_id} WHERE id = #{@id};")
+  end
 end
--- spec/patient_spec.rb
+++ spec/patient_spec.rb
@@ -2,6 +2,11 @@ require 'spec_helper'
 
 describe '#Patient' do
 
+before(:each)do 
+  @doctor = Doctor.new({:name => "Fluffy", :specialty => "whiskers", :id=> nil})
+  @doctor.save()
+end 
+
   describe('.all') do
     it("returns an empty array when there are no patients") do
       expect(Patient.all).to(eq([]))
@@ -10,11 +15,21 @@ describe '#Patient' do
 
   describe('#save') do
     it("saves a doctor") do
-      patient = Patient.new({:name => "Richmond", :id => nil, :birthday => "1992-02-07", :doctor_id => nil})
+      patient = Patient.new({:name => "Richmond", :id => nil, :birthday => "1992-02-07", :doctor_id => @doctor.id})
       patient.save()
-      patient2 = Patient.new({:name => "Dave", :id => nil, :birthday => "1990-09-03", :doctor_id => nil})
+      patient2 = Patient.new({:name => "Dave", :id => nil, :birthday => "1990-09-03", :doctor_id => @doctor.id})
       patient2.save()
       expect(Patient.all).to(eq([patient, patient2]))
+    end
+  end
+
+  describe('#update') do
+    it("updates patient's doctor") do
+      patient = Patient.new({:name => "Richmond", :id => nil, :birthday => "1992-02-07", :doctor_id => @doctor.id})
+      patient.save()
+      # doctor = Doctor.new({:name => "Richmond", :id => nil, :specialty => "cancer"})
+      patient.update({:name => patient.name, :doctor_id => @doctor.id})
+      expect(patient.doctor_id).to(eq(@doctor.id))
     end
   end
 end
